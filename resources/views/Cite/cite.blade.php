1bdcc7591d837808569d6a9ffcdd31e0c301baab
Anthony <logicainformatica18@gmail.com>
2025-02-19 09:16:50 -0500

    quitar acordeon

--- resources/views/Cite/cite.blade.php
+++ resources/views/Cite/cite.blade.php
@@ -59,7 +59,7 @@
                     </div>
                 </div>
 
-                <div class="accordion" id="accordionExample">
+                {{-- <div class="accordion" id="accordionExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header">
                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne">
@@ -158,7 +158,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </form >
             </div>
 
